b253ffae9c195f15a51d988f45df807e6669e085
Adam Hao <haozg44@outlook.com>
2024-01-09 23:22:29 +0800

    Minor change.

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 /// <reference types="vite/client" />
 
 // Add data types to window.navigator ambiently for implicit use in the entire project. See https://www.typescriptlang.org/docs/handbook/triple-slash-directives.html#-reference-types- for more info.
